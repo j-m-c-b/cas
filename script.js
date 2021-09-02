4d85db013bb9eb8ba92f9d50ff99c3c56864fcbe
j-m-c-b <25614891+j-m-c-b@users.noreply.github.com>
2021-09-02 03:15:14 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ var currentPeer = null;
 var screenSharing = false;
 let host = false;
 document.getElementById("room-input").value = "";
+
+let urlParams = new URLSearchParams(window.location.search);
+let idURL = urlParams.get('id');
+
+if (idURL) {
+    document.getElementById("room-input").value = idURL;
+    joinRoom();
+}
 function createRoom() {
     host = true;
     console.log("Creating Room")
     let room = document.getElementById("room-input").value;
+
+
     // if (room == " " || room == "") {
     //     alert("Please enter room number")
     //     return;
@@ -23,7 +33,9 @@ function createRoom() {
     peer.on('open', (id) => {
         console.log("Peer Connected with ID: ", id)
         hideModal()
-        prompt("id", id)
+        console.log(`${window.location.href}?id=${id}`)
+        document.getElementById("linkId").innerText = `${window.location.href}?id=${id}`;
+        createQR(`${window.location.href}?id=${id}`);
         // getUserMedia({ video: false, audio: false }, (stream) => {
         //     local_stream = stream;
         //     setLocalStream(local_stream)
@@ -80,8 +92,8 @@ function joinRoom() {
     peer = new Peer()
     peer.on('open', (id) => {
         console.log("Connected with Id: " + id)
-  
-        getUserMedia({ video:  { facingMode: { exact: "environment" } } , audio: false }, (stream) => {
+
+        getUserMedia({ video: { facingMode: { exact: "environment" } }, audio: false }, (stream) => {
             local_stream = stream;
             setLocalStream(local_stream)
             notify("Joining peer")
@@ -98,5 +110,14 @@ function joinRoom() {
 }
 
 function record() {
+
+}
+function createQR(id){
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+        text: id,
+        width: 128,
+        height: 128,
+        correctLevel: QRCode.CorrectLevel.L
+    });
 
 }
